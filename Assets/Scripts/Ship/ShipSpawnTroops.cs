bf5b237fb298ac9092b999a789212c4e25f5ef04
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-15 00:01:18 +0100

    ships move every X seconds and spawn Y troops.

---
+++ Assets/Scripts/Ship/ShipSpawnTroops.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+
+public class ShipSpawnTroops : MonoBehaviour
+{
+    [SerializeField] private float waitTillSpawn;
+    private float remainingWaitTime = 0f;
+    [SerializeField] private Vector3[] spawnPositions;
+    [SerializeField] private float spawnCooldown;
+    private float remainingCooldownTime = 0f;
+    [SerializeField] private float animationTime;
+    private float remainingAnimationTime = 0f;
+    [SerializeField]private Animator _animator;
+    [SerializeField]private GameObject troopPrefab;
+
+
+    private void Update()
+    {
+        // Off cooldown
+        if (remainingCooldownTime <= 0)
+        {
+            remainingWaitTime = waitTillSpawn;
+            remainingCooldownTime = spawnCooldown;
+            remainingAnimationTime = animationTime;
+            _animator.SetBool("Moving", true);
+            Debug.Log("Starting Animation");
+        }
+        // At point which we spawn troops
+        if (remainingWaitTime <= 0)
+        {
+            // Spawn
+            Spawn();
+            remainingWaitTime = waitTillSpawn + animationTime + remainingCooldownTime;
+        }
+        // Animation finished
+        if (remainingAnimationTime <= 0)
+        {
+            _animator.SetBool("Moving", false);
+            Debug.Log("Stopping Animation");
+            // Reset cooldown
+            remainingCooldownTime = spawnCooldown;
+            remainingAnimationTime = animationTime + remainingCooldownTime;
+        }
+    }
+
+    private void Spawn()
+    {
+        for (int i = 0; i < spawnPositions.Length; i++)
+        {
+            GameObject troopGameObject = troopPrefab;
+            troopGameObject.transform.position = spawnPositions[i];
+            Instantiate(troopGameObject);
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        if (remainingWaitTime > 0)
+        {
+            remainingWaitTime -= Time.deltaTime;
+        }
+
+        if (remainingCooldownTime > 0)
+        {
+            remainingCooldownTime -= Time.deltaTime;
+        }
+
+        if (remainingAnimationTime > 0)
+        {
+            remainingAnimationTime -= Time.deltaTime;
+        }
+    }
+}
