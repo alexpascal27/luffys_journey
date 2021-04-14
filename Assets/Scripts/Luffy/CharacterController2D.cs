303301d6fb2f8ab5e06b2c31684cb19a92c37ef0
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-14 15:28:43 +0100

    Initial commit

---
+++ Assets/Scripts/Luffy/CharacterController2D.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f; // How much to smooth out the movement
+	private Rigidbody2D m_Rigidbody2D;
+	private bool m_FacingRight = true; // For determining which way the player is currently facing.
+	private Vector3 m_Velocity = Vector3.zero;
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+
+	public void Move(float horizontalMove, float verticalMove)
+	{
+		MoveHorizontally(horizontalMove);
+		MoveVertically(verticalMove);
+	}
+
+	private void MoveHorizontally(float move)
+	{
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(move, m_Rigidbody2D.velocity.y);
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity =
+			Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+		// If the input is moving the player right and the player is facing left or vice versa
+		if ((move > 0 && !m_FacingRight) || (move < 0 && m_FacingRight))
+		{
+			// ... flip the player.
+			Flip();
+		}
+	}
+
+	private void MoveVertically(float move)
+	{
+		// Move the character by finding the target velocity
+		Vector3 targetVelocity = new Vector2(m_Rigidbody2D.velocity.x, move);
+		// And then smoothing it out and applying it to the character
+		m_Rigidbody2D.velocity =
+			Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+	}
+
+
+private void Flip()
+	{
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+
+		// Transform using Rotation
+		transform.Rotate(0f, 180f, 0f);
+	}
+}
---
+++ Assets/Scripts/Luffy/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using UnityEngine;
+using Random = System.Random;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Animation
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float punchAnimationTime = 1f;
+    private float currentPunchAnimationTime = 0f;
+    private bool currentlyPunching = false;
+    
+    // Movement
+    public CharacterController2D controller;
+    
+    [SerializeField][Range(0f,1000f)]private float runSpeed = 50f;
+    float _horizontalMove = 0f;
+    float _verticalMove = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (currentPunchAnimationTime <= 0)
+            {
+                _animator.SetBool("Attacking", true);
+                currentPunchAnimationTime = punchAnimationTime;
+                currentlyPunching = true;
+            }
+        }
+
+        if (currentlyPunching && currentPunchAnimationTime <= 0)
+        {
+            _animator.SetBool("Attacking", false);
+            currentlyPunching = false;
+        }
+        
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        _verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(_horizontalMove * Time.fixedDeltaTime, _verticalMove*Time.fixedDeltaTime);
+        if (_horizontalMove != 0 || _verticalMove != 0)
+        {
+            _animator.SetBool("Running", true);
+        }
+        else
+        {
+            _animator.SetBool("Running", false);
+        }
+        
+        if (currentPunchAnimationTime > 0)
+        {
+            currentPunchAnimationTime -= Time.deltaTime;
+        }
+    }
+}
