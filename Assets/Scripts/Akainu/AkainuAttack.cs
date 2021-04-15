a7db32cb722a2095385cb2ec4704f6db46a612b6
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-15 01:25:13 +0100

    basic ability 1 animation akainu done.

--- Assets/Scripts/Akainu/AkainuAttack.cs
+++ Assets/Scripts/Akainu/AkainuAttack.cs
@@ -1,15 +1,28 @@
-using System;
+﻿using System;
 using UnityEngine;
 public class AkainuAttack : MonoBehaviour
 {
     [SerializeField] [Range(0f, 100f)] private float ability1Cooldown;
     [SerializeField] private float arriveAnimationTime = 6f;
     [SerializeField] private float ability1AnimationTime;
+    [SerializeField] private Vector3 ability1Spawn;
     private bool ability1Attack = false;
     private float currentCooldown;
     private float currentAbility1AnimationTime = 0f;
 
-    [SerializeField] private Animator _animator;
+    [SerializeField] private float periodBetweenWarningAndAflame;
+    private float remainingWait = 0f;
+    private bool waiting = false;
+    [SerializeField] private float warningAnimationPeriod;
+    private float remainingWarningAnimationPeriod = 0f;
+    private bool warningAnimating = false;
+    [SerializeField] private float cellAflameAnimationPeriod;
+    private bool aflameAnimating = false;
+    private float remainingCellAflameAnimationPeriod = 0f;
+    
+
+    [SerializeField] private Animator _akainuAnimator;
+    [SerializeField] private Animator _ability1Animator;
 
     private void Start()
     {
@@ -23,7 +36,35 @@ public class AkainuAttack : MonoBehaviour
         {
             ability1Attack = true;
             currentAbility1AnimationTime = ability1AnimationTime;
-            _animator.SetBool("PunchingGround", true);
+            _akainuAnimator.SetBool("PunchingGround", true);
+        }
+        // Warning finished
+        if (warningAnimating && remainingWarningAnimationPeriod <= 0)
+        {
+            warningAnimating = false;
+            _ability1Animator.SetBool("Warning" , false);
+
+            waiting = true;
+            remainingWait = periodBetweenWarningAndAflame;
+        }
+        // Waiting between warning and flame finished
+        if (waiting && remainingWait <= 0)
+        {
+            waiting = false;
+
+            aflameAnimating = true;
+            _ability1Animator.SetBool("Flame" , true);
+            remainingCellAflameAnimationPeriod = cellAflameAnimationPeriod;
+        }
+        // Finished cell blazing
+        if (aflameAnimating && remainingCellAflameAnimationPeriod <= 0)
+        {
+            aflameAnimating = false;
+            _ability1Animator.SetBool("Flame" , false);
+            
+            currentCooldown = ability1Cooldown;
+            _akainuAnimator.SetBool("PunchingGround", false);
+            ability1Attack = false;
         }
     }
 
@@ -34,6 +75,21 @@ public class AkainuAttack : MonoBehaviour
         {
             currentCooldown -= Time.deltaTime;
         }
+
+        if (warningAnimating)
+        {
+            remainingWarningAnimationPeriod -= Time.deltaTime;
+        }
+
+        if (aflameAnimating)
+        {
+            remainingCellAflameAnimationPeriod -= Time.deltaTime;
+        }
+
+        if (waiting)
+        {
+            remainingWait -= Time.deltaTime;
+        }
         
         // Ability 1 Animation
         if (currentAbility1AnimationTime > 0)
@@ -42,9 +98,14 @@ public class AkainuAttack : MonoBehaviour
         }
         if (ability1Attack && currentAbility1AnimationTime <= 0)
         {
-            _animator.SetBool("PunchingGround", false);
+            _akainuAnimator.SetBool("PunchingGround", false);
             ability1Attack = false;
             currentCooldown = ability1Cooldown;
+            
+            // Show warning
+            remainingWarningAnimationPeriod = warningAnimationPeriod;
+            warningAnimating = true;
+            _ability1Animator.SetBool("Warning" , true);
         }
     }
 }
